947725898112e2814aa26fe630251982eaf315d0
adriq4 <adri230992@gmail.com>
2025-02-11 07:57:39 +0100

    flex-grid created with css and hover effect done

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const cntr = document.querySelector(".container");
+const $container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  let crtDiv = document.createElement("div");
-  crtDiv.style.width = "60px";
-  crtDiv.style.height = "60px";
-  crtDiv.style.color = "blue";
-  crtDiv.style.backgroundColor = "grey";
-  crtDiv.style.border = "1px solid #fff";
-  crtDiv.textContent = +i;
-  cntr.appendChild(crtDiv);
+let gridSize = 16;
+
+function gridContainer(size) {
+  const totalGrids = size * size;
+  const containerSize = 960 / size;
+  for (let i = 0; i < totalGrids; i++) {
+    let createGrid = document.createElement("div");
+
+    createGrid.style.backgroundColor = "#cccccc";
+    createGrid.style.border = "1px solid #fff";
+    createGrid.style.width = `${containerSize}px`;
+    createGrid.style.height = `${containerSize}px`;
+
+    createGrid.addEventListener("mouseover", (e) => {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      e.target.style.backgroundColor = `#${randomColor}`;
+    });
+    createGrid.addEventListener("mouseout", () => {
+      createGrid.style.backgroundColor = "#cccccc";
+    });
+    $container.appendChild(createGrid);
+  }
 }
+gridContainer(16);
